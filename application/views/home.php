6f40fc4f6c3e0eaf5191514c06f8029a204a7147
Muhamadhabib <muhamad.habib08@gmail.com>
2019-04-03 21:39:43 +0700

    perbaikan merge 2

--- application/views/home.php
+++ application/views/home.php
@@ -67,29 +67,7 @@
         </div>
 
         <!-- Widgets -->
-        <div class="row clearfix">
-            <div class="col-lg-4 col-sm-6 col-xs-12">               
-                <div class="info-box bg-pink hover-expand-effect">
-                    <div class="icon">
-                        <i class="material-icons">store</i>
-                    </div>
-                    <div class="content">
-                        <div class="text">PENJUALAN</div>
-                        <div class="number count-to" data-from="0" data-to="125" data-speed="1000" data-fresh-interval="20"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-sm-6 col-xs-12">
-                <div class="info-box bg-indigo hover-expand-effect">
-                    <div class="icon">
-                        <i class="material-icons">receipt</i>
-                    </div>
-                    <div class="content">
-                        <div class="text">KREDIT</div>
-                        <div class="number count-to" data-from="0" data-to="125" data-speed="1000" data-fresh-interval="20"></div>
-                    </div>
-                </div>
-            </div>
+        
             <div class="col-lg-4 col-sm-6 col-xs-12">
                 <div class="panel panel-primary">
                     <div class="panel-heading">
@@ -117,17 +95,7 @@
                     </a>
                 </div>
             </div>
-            <div class="col-lg-4 col-sm-6 col-xs-12">
-                <div class="info-box bg-cyan hover-expand-effect">
-                    <div class="icon">
-                        <i class="material-icons">list</i>
-                    </div>
-                    <div class="content">
-                        <div class="text">BARANG</div>
-                        <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20"></div>
-                    </div>
-                </div>
-            </div>
+           
             <div class="col-lg-4 col-sm-6 col-xs-12">
                 <div class="panel panel-alert">
                     <div class="panel-heading">
@@ -153,28 +121,6 @@
                             </div>
                         </div>
                     </a>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-3 col-sm-6 col-xs-12">
-                <div class="info-box bg-light-green hover-expand-effect">
-                    <div class="icon">
-                        <i class="material-icons">person</i>
-                    </div>
-                    <div class="content">
-                        <div class="text">PELANGGAN</div>
-                        <div class="number count-to" data-from="0" data-to="243" data-speed="1000" data-fresh-interval="20"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-3 col-sm-6 col-xs-12">
-                <div class="info-box bg-orange hover-expand-effect">
-                    <div class="icon">
-                        <i class="material-icons">shopping_cart</i>
-                    </div>
-                    <div class="content">
-                        <div class="text">TRANSAKSI</div>
-                        <div class="number count-to" data-from="0" data-to="1225" data-speed="1000" data-fresh-interval="20"></div>
-                    </div>
                 </div>
             </div>
             
