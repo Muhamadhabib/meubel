f232dc826f8ed86978c23512ee1840be7c908640
Muhamadhabib <muhamad.habib08@gmail.com>
2019-05-03 00:38:35 +0700

    tambah logout & perbaikan dashboard

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,11 +3,14 @@
         public function __construct()
         {
             parent::__construct();
-        
         }
         public function index(){
+            $data['kredit'] = $this->db->get('kredit')->num_rows();
+            $data['tunai'] = $this->db->get('trans')->num_rows();
+            $data['brg'] = $this->db->get('barang')->num_rows();
+            $data['plg'] = $this->db->get('pelanggan')->num_rows();
             $this->load->view("header");
-            $this->load->view("home");
+            $this->load->view("home",$data);
             $this->load->view("footer");
         }
     }
